1be3e2e38dde70d9c6090dc2a58c6215bf28cf78
ZameerNoori <zameer.noori3@gmail.com>
2022-05-31 21:36:02 -0400

    1st commit

---
+++ src/gitReview/gitreview01.java
@@ -0,0 +1,13 @@
+package gitReview;
+
+public class gitreview01 {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+				System.out.print("today we learn github");
+				System.out.print("today is a very good dAY");
+
+	}
+
+}
